cb420cc9b83ea72163fd531777102416fb9b9c3a
Ramon <ramon.r.santana@gmail.com>
2023-04-14 20:03:38 +0200

    feat: add service migrations, seeders and models tested is ok

---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('services')->insert(
+            [
+                [
+                    'id' => 1,
+                    'duration' => "40min",
+                    'price' => "40",
+                    'description' => "Bathroom with the best products on the market",
+                    'type' => "Bath"
+                ],
+                [
+                    'id' => 2,
+                    'duration' => "60min",
+                    'price' => "80",
+                    'description' => "Bathroom with the best products on the market",
+                    'type' => "Nails Cutting"
+                ],
+                [
+                    'id' => 3,
+                    'duration' => "80min",
+                    'price' => "120",
+                    'description' => "We have the best haircutting              professionals, internationally recognized.",
+                    'type' => "Haircut"
+                ],
+                [
+                    'id' => 4,
+                    'duration' => "30min",
+                    'price' => "30",
+                    'description' => "We know that the most important thing is your pet's teeth, that's why we take care of their oral hygiene.",
+                    'type' => "Toothbrushing"
+                ]
+            ]
+        );
+    }
+}
